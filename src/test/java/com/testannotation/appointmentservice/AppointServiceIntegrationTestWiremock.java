5a73559de2eaf9861abfea487c5174d846080a12
Pradip patra <pradip.patra@gmail.com>
2021-08-03 09:52:51 +0530

    day1_begin

--- src/test/java/com/testannotation/appointmentservice/AppointServiceIntegrationTestWiremock.java
+++ src/test/java/com/testannotation/appointmentservice/AppointServiceIntegrationTestWiremock.java
@@ -2,25 +2,25 @@ package com.testannotation.appointmentservice;
 
 import com.testannotation.appointmentservice.client.PatientClient;
 import com.testannotation.appointmentservice.model.Patient;
-import org.junit.Before;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock;
 import org.springframework.http.ResponseEntity;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import static com.github.tomakehurst.wiremock.client.WireMock.*;
 import static org.assertj.core.api.BDDAssertions.then;
 
-@RunWith(SpringRunner.class)
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
 @AutoConfigureWireMock (port = 9090)
 public class AppointServiceIntegrationTestWiremock {
     @Autowired
     PatientClient patientClient;
 
-    @Before
+    @BeforeEach
     public void createStub(){
         stubFor(get(urlPathEqualTo("/patient-service/patient"))
                 .withQueryParam("MRN", equalTo("2009120409"))
@@ -31,7 +31,7 @@ public class AppointServiceIntegrationTestWiremock {
                             "\"firstName\": \"John\",\n"+
                             "\"lastName\": \"Smart\",\n"+
                             "\"birthDate\": \"02/02/1997\",\n"+
-                            "\"registration\": \"04/12/2009\",\n"+
+                            "\"dateOfRegistration\": \"04/12/2009\",\n"+
                             "\"address\": \"15 Foreshore Road, Philadelphia, PA, 19101\",\n"+
                             "\"mrn\": \"2009120401\"\n"+
                              "}")
@@ -40,7 +40,7 @@ public class AppointServiceIntegrationTestWiremock {
     }
 
     @Test
-    public void scheduleAppointment(){
+    public void givenMRN_whenSearchForPatient_shouldReturnAPatient(){
         //Given
         String MRN = "2009120409";
 
